222bffaa8f2a207d6062dcd3e34275e3aa51b027
Albert XU <xyb1114@gmail.com>
2019-08-23 17:38:31 +0200

    fixup

--- react-graphql-github-vanilla/src/App.js
+++ react-graphql-github-vanilla/src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const GET_ORGANIZATION = `
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
+
 const axiosGitHubGraphQL = axios.create({
   baseURL: 'https://api.github.com/graphql',
   headers: {
@@ -11,10 +20,49 @@ const axiosGitHubGraphQL = axios.create({
 const TITLE = 'React GraphQL GitHub Client';
 
 class App extends Component {
+
+  state = {
+    path: 'the-road-to-learn-react/the-road-to-learn-react',
+  };
+
+  componentDidMount() {
+    this.onFetchFromGithub()
+  }
+
+  onChange = event => {
+    this.setState({ path: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+  };
+
+  onFetchFromGithub = () => {
+    axiosGitHubGraphQL.post('', {query: GET_ORGANIZATION}).then(result => console.warn('alb',{result}))
+  }
+
   render() {
+    const { path } = this.state;
     return (
       <div>
         <h1>{TITLE}</h1>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">
+            Show open issues for https://github.com/
+          </label>
+          <input
+            id="url"
+            type="text"
+            onChange={this.onChange}
+            style={{ width: '300px' }}
+            value={path}
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        <hr />
+
+        {/* Here comes the result! */}
       </div>
     );
   }
